7009821e31e5efbcce290585323d524f51d30af5
Thercules <thhercules2012@gmail.com>
2023-05-23 00:33:25 -0300

    First API commit

---
+++ src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,27 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.erudio.config.FileStorageConfig;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
+@EnableAutoConfiguration
+@ComponentScan
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+		
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+		String result = bCryptPasswordEncoder.encode("humberto11");
+		System.out.println("My hash " + result);
+	}
+}
